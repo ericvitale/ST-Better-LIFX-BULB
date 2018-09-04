560c2da725fb4a71a708c7b5d382a0125e2eadd0
ericvitale <ericvitale@gmail.com>
2018-09-04 19:30:37 -0400

    Update better-lifx-bulb-plus.groovy

--- devicetypes/ericvitale/better-lifx-bulb-plus.src/better-lifx-bulb-plus.groovy
+++ devicetypes/ericvitale/better-lifx-bulb-plus.src/better-lifx-bulb-plus.groovy
@@ -3,6 +3,7 @@
  *
  *  Copyright 2016 Eric Vitale
  *
+ *  Version 1.1.5a - Fixed typo in vid setting. (09/04/2018)
  *  Version 1.1.5 - Updated to support the new SmartThings mobile app. (08/20/2018)
  *  Version 1.1.4 - Updated the "on" indicator color of the DH to the standard ST Blue (#00a0dc). (08/03/2018)
  *  Version 1.1.3 - Cleaned up a bit. (06/30/2017) 
@@ -36,7 +37,7 @@ include 'asynchttp_v1'
 import java.text.DecimalFormat;
  
 metadata {
-	definition (name: "Better LIFX Bulb Plus", namespace: "ericvitale", author: "ericvitale@gmail.com", vid: "generic-rgbw-color-bulb) {
+	definition (name: "Better LIFX Bulb Plus", namespace: "ericvitale", author: "ericvitale@gmail.com", vid: "generic-rgbw-color-bulb") {
 		capability "Actuator"
 		capability "Color Control"
 		capability "Color Temperature"
